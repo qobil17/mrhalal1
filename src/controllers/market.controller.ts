efb8163d053869d3e6a1958d091647a24ee0c0d8
qobil17 <ibrohimovqobilbek5@gmail.com>
2025-07-04 18:47:53 +0900

    feat: develop member schema model

--- src/controllers/market.controller.ts
+++ src/controllers/market.controller.ts
@@ -1,5 +1,14 @@
+import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 
 const marketController: T = {};
+
+marketController.goHome = (req: Request, res: Response) => {
+  try {
+    res.send("Home Page");
+  } catch (err) {
+    console.log("Error, goHome");
+  }
+};
 
 export default marketController;
--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -6,9 +6,9 @@ import marketController from "./controllers/market.controller";
 
 /**Restaurant */
 routerAdmin.get("/", marketController.goHome);
-routerAdmin
-  .get("/login", marketController.getLogin)
-  .post("/login", marketController.processLogin);
+// routerAdmin
+//   .get("/login", marketController.getLogin)
+//   .post("/login", marketController.processLogin);
 // routerAdmin
 //   .get("/signup", marketController.getSignup)
 //   .post(
@@ -16,14 +16,14 @@ routerAdmin
 //     makeUploader("members").single("memberImage"),
 //     marketController.processSignup
 //   );
-routerAdmin.get("/logout", marketController.logout);
-routerAdmin.get("/check-me", marketController.checkAuthSession);
+// routerAdmin.get("/logout", marketController.logout);
+// routerAdmin.get("/check-me", marketController.checkAuthSession);
 /**Product */
-routerAdmin.get(
-  "/product/all",
-  marketController.verifyRestaurant,
-  marketController.getAllProducts
-);
+// routerAdmin.get(
+//   "/product/all",
+//   marketController.verifyRestaurant,
+//   marketController.getAllProducts
+// );
 // routerAdmin.post(
 //   "/product/create",
 //   marketController.verifyRestaurant,
@@ -36,16 +36,16 @@ routerAdmin.get(
 //   productController.updateChosenProduct
 // );
 /**User */
-routerAdmin.get(
-  "/user/all",
-  marketController.verifyRestaurant,
-  marketController.getUsers
-);
-routerAdmin.post(
-  "/user/edit",
-  marketController.verifyRestaurant,
-  marketController.upadateChosenUser
-);
+// routerAdmin.get(
+//   "/user/all",
+//   marketController.verifyRestaurant,
+//   marketController.getUsers
+// );
+// routerAdmin.post(
+//   "/user/edit",
+//   marketController.verifyRestaurant,
+//   marketController.upadateChosenUser
+// );
 
 /**PRODUCT */
 
